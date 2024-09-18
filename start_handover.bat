b6b55974c0991d5e873692ee1f233dfda397fd46
tony <tony@ying31.com>
2024-09-18 19:43:48 +0800

    add script

---
+++ start_handover.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: 定義源路徑和目標路徑
+set DESTINATION_PATH=C:\handover_api\output\net6.0\win-x64
+
+:: 刪除原先的appsettings.json文件（如果存在）
+if exist "%DESTINATION_PATH%\appsettings.json" (
+    del "%DESTINATION_PATH%\appsettings.json"
+    echo appsettings.json deleted
+) else (
+    echo appsettings.json not exist
+)
+
+:: 將appsettings_KimForest.json重命名為appsettings.json
+if exist "%DESTINATION_PATH%\appsettings_KimForest.json" (
+    copy "%DESTINATION_PATH%\appsettings_KimForest.json" "%DESTINATION_PATH%\appsettings.json"
+    echo copied appsettings_KimForest.json to appsettings.json
+) else (
+    echo appsettings_KimForest.json not exist
+    pause
+    exit /b 1
+)
+
+:: 切換到目標目錄並運行 exe 文件
+cd "%DESTINATION_PATH%"
+start "" "handover_api.exe"
+
+pause
