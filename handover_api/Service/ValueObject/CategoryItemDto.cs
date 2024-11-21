9ecb6d5e36bcafdf16aea50cf644682f62c285a7
Yung-Yu-Lin <wfjh10602@gmail.com>
2024-11-21 14:55:09 +0800

    給予 itemoption values 初始值

--- handover_api/Service/ValueObject/CategoryItemDto.cs
+++ handover_api/Service/ValueObject/CategoryItemDto.cs
@@ -23,10 +23,10 @@ namespace handover_api.Service.ValueObject
 
     public class ItemOptionValues
     {
-        public List<string>? optionValue {get;set;}
+        public List<string> optionValue {get;set;} = new List<string>();
         public string? commentValue {get;set;}
-        public List<string>? tableValue {get;set;}
-        public List<string>? remarkAssignUserNameValue {get;set;}
-        public List<string>? remarkAssignUserIDValue {get;set;}
+        public List<string> tableValue {get;set;} = new List<string>();
+        public List<string> remarkAssignUserNameValue {get;set;} = new List<string>();
+        public List<string> remarkAssignUserIDValue {get;set;} = new List<string>();
     }
 }
